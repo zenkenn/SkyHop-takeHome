36b80c86ffb4232ee45fb48d83598248884d4c5f
zenkenn <kenneth.riveron@gmail.com>
2024-02-19 22:04:16 -0500

    Created file structure

---
+++ skyHopTakeHome/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './Components/HeaderSection/HeaderSection'
+import HeaderSection from './Components/HeaderSection/HeaderSection';
+
+function App() {
+  return (
+    <div className='root'>
+      <HeaderSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ skyHopTakeHome/src/Components/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,9 @@
+import './HeaderSection.css'
+
+const HeaderSection = () => {
+  return (
+    <div>HeaderSection</div>
+  )
+}
+
+export default HeaderSection
