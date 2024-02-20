6502af6b1484962031a71f13ab34f2c2aa46de28
zenkenn <kenneth.riveron@gmail.com>
2024-02-19 23:33:32 -0500

    commit

--- skyHopTakeHome/src/App.tsx
+++ skyHopTakeHome/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
-import './Components/HeaderSection/HeaderSection'
-import HeaderSection from './Components/HeaderSection/HeaderSection';
+import "./App.css";
+import "./Components/HeaderSection/HeaderSection";
+import HeaderSection from "./Components/HeaderSection/HeaderSection";
 
 function App() {
   return (
-    <div className='root'>
-      <HeaderSection />
+    <div className="root">
+      <div className="container">
+        <HeaderSection />
+      </div>
     </div>
   );
 }
