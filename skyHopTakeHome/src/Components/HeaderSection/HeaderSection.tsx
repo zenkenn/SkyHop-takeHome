2ca15f202a9122de265f15f4798209e5430d1f7c
zenkenn <kenneth.riveron@gmail.com>
2024-02-19 23:33:17 -0500

    Created Header Section

--- skyHopTakeHome/src/Components/HeaderSection/HeaderSection.tsx
+++ skyHopTakeHome/src/Components/HeaderSection/HeaderSection.tsx
@@ -1,9 +1,16 @@
-import './HeaderSection.css'
+import "./HeaderSection.css";
 
 const HeaderSection = () => {
   return (
-    <div>HeaderSection</div>
-  )
-}
+    <div className="headerSectionRoot">
+      <div className="exitButtonContainer">
+        <button className="exitButton">X</button>
+      </div>
+      <div className="HeaderContainer">
+        <div className="Header">Document Upload</div>
+      </div>
+    </div>
+  );
+};
 
-export default HeaderSection
+export default HeaderSection;
